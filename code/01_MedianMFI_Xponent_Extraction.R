21f79de2188d3895578f63fc6e5f78ad21a00cae
Sahal Thahir <sahal@SOMPEDSP47T70P4Y6.local>
2024-03-27 16:20:36 -0400

    Loop works for raw Xponent csvs, added all csv files to data/raw/luminex. Updated gitignore to not include csv files.

--- code/01_MedianMFI_Xponent_Extraction.R
+++ code/01_MedianMFI_Xponent_Extraction.R
@@ -1,83 +1,59 @@
-# Create Median MFI data from Xponent 
+# Pulling Median MFI from Xponent csv file. Please see datakey for GoogleDrive/R conversion. 
+# Keep raw data files within the Luminex/raw data folder.  
 
-#Extraction of Median MFI data (create median_df)----
 
-  # Read the CSV file line by line
-  lines <- readLines("/Users/sahal/Documents/R Projects/RTSS_Kisumu_Schisto/data/Chul.(SET2)_RTSS_25_Mar_2024_TotalIgG_AM.csv")
-  
-  # Remove null lines
-  lines <- lines[lines != ""]
-  
-  # Find indices of rows containing "Median" and blank rows
-  median_indices <- grep("Median", lines)
-  blank_indices <- grep("^$", lines)
-  
-  # Find the index of the first blank row after each "Median" row
-  median_blank_indices <- sapply(median_indices, function(median_index) {
-    next_blank_index <- which(blank_indices > median_index)[1]
-    if (!is.na(next_blank_index)) {
-      blank_indices[next_blank_index]
-    } else {
-      length(lines) + 1
-    }
-  })
-  
-  # Extract data between "Median" and blank rows
-  data_between_medians <- lapply(seq_along(median_indices), function(i) {
-    start_index <- median_indices[i] + 1
-    end_index <- median_blank_indices[i] - 1
-    if (end_index >= start_index) {
-      data <- lines[start_index:end_index]
-      data <- read.csv(text = paste(data, collapse = "\n"), stringsAsFactors = FALSE)
+# Directory (raw Xponent data) and Loop initiation ----
+directory <- "/Users/sahal/Documents/R Projects/RTSS_Kisumu_Schisto/data/raw/luminex/"
+
+    # Get list of CSV files in the directory
+    csv_files <- list.files(directory, pattern = "\\.csv$", full.names = TRUE)
+    
+    # Loop through each CSV file
+    for (file_path in csv_files) {
+
+# File Extraction, initial triming for MFI data ---- 
+      lines <- readLines(file_path)
       
-      # Check if the Location column contains a blank value
-      if ("" %in% data$Location) {
-        end_index <- which(data$Location == "")[1] - 1
-        if (is.na(end_index)) {
-          end_index <- nrow(data)
-        }
-        data <- data[1:end_index, ]
-      }
+  # Median MFI trimming by rows: Below "Median" and two above "Net MFI"    
+            # Find the row index containing the word "Median"
+            median_row_index <- grep("Median", lines)
+            
+            # Find the row index containing the word "Net MFI"
+            net_mfi_row_index <- grep("Net MFI", lines)
+            
+            # Find the row index of two rows above "Net MFI"
+            net_mfi_row_index <- net_mfi_row_index - 2
       
-      data
-    } else {
-      NULL
-    }
-  })
+            # Extract data between "Median" and two rows above "Net MFI"
+            data_lines <- lines[(median_row_index + 1):net_mfi_row_index]
+      
+      # Create a data frame
+      results_df <- read.csv(text = paste(data_lines, collapse = "\n"), na.strings = "")
+                
+#Trimming results_data (remove well, Total reactions), adding Plate column, ----
+  # Filter out columns between "Location" and "Total.Events"
+        location_index <- grep("Location", colnames(results_df))
+        total_events_index <- grep("Total.Events", colnames(results_df))
+        columns_to_keep <- (location_index + 1):(total_events_index - 1)
+        
+        results_df <- results_df[, columns_to_keep]
   
-  # Remove NULL entries
-  data_between_medians <- Filter(Negate(is.null), data_between_medians)
-  
-  # Combine all dataframes into a single dataframe
-  median_df <- do.call(rbind, data_between_medians)
-  
-  # Print or use result_df as needed
-  print(median_df)
+  # Add a column named "Plate" containing the CSV file name without the extension
+          file_name <- basename(file_path)
+          file_name_without_ext <- tools::file_path_sans_ext(file_name)
+    results_df$Plate <- file_name_without_ext
 
-# Data extraction (create results_df, subtraction of BSA MFI) ---- 
-  # dataframe of interest (Samples + Antigens)
-      # Find the indices of "Location" and "Total.Events" columns
-      location_index <- which(colnames(median_df) == "Location")
-      total_events_index <- which(colnames(median_df) == "Total.Events")
+# File Export w/ Plate names in data/clean/luminex----
+    
+  # Extract filename without extension
+        file_name <- basename(file_path)
+        file_name_without_ext <- tools::file_path_sans_ext(file_name)
+
+  # Rename the data frame with the filename included
+      assign(paste0(file_name_without_ext, "_results_df"), results_df)
       
-      # Extract the desired columns from result_df
-      results_df <- median_df[, (location_index + 1):(total_events_index - 1)]
-      
-      # Print or use desired_columns as needed
-      print(results_df)
-
-  # BSA subtraction
-      # Find the index of the "BSA" column
-      bsa_index <- which(colnames(results_df) == "BSA")
-      
-      # Subtract the "BSA" column from all numerical columns
-      results_df[, -bsa_index] <- results_df[, -bsa_index] - results_df[, bsa_index]
-      
-      # Print or use the modified result_df as needed
-      print(results_df)
-      
-
-
-
-
-
+  # Export the final data frame to clean data/luminex
+      export_path <- "/Users/sahal/Documents/R Projects/RTSS_Kisumu_Schisto/data/clean/luminex/"
+      export_file <- paste0(export_path, file_name_without_ext, "_results_df.csv")
+      write.csv(results_df, file = export_file, row.names = FALSE)
+} #ending loop
